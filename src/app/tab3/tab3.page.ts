56c550e9c6de5163aaccce34e9ddd30d70b82a4e
renancarrillo <renancarrillo@gmail.com>
2022-03-29 11:39:34 -0700

    Cambios en el modal

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  mensaje: any;
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+  alert_btn(from){
+    if(from == "boton1"){
+      this.basic_alert("Vengo del boton1","Esto es el mensaje del boton1");
+    }else{
+      this.basic_alert("Vengo del boton2","Esto es el mensaje del boton2");
+    }
+  }
+
+  async basic_alert(variable,variable2) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: variable,
+      message: variable2,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 }
